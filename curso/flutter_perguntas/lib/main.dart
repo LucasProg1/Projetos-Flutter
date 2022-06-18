ec8c85509b5b3a3fa3320fe26b899836de5e3293
Lucas Pereira Brito <lucasbritto.lpb@gmail.com>
2022-06-18 12:18:24 -0400

    curso

---
+++ curso/flutter_perguntas/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import './resultado.dart';
+import './questionario.dart';
+
+main() => runApp(new PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  var _notaTotal = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'nota': 10},
+        {'texto': 'Vermelho', 'nota': 5},
+        {'texto': 'Rosa', 'nota': 3},
+        {'texto': 'Azul', 'nota': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Onça-Pintada', 'nota': 10},
+        {'texto': 'Papagaio', 'nota': 5},
+        {'texto': 'Arara', 'nota': 3},
+        {'texto': 'Cachoorrooo hau hau', 'nota': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': [
+        {'texto': 'Maria', 'nota': 3},
+        {'texto': 'João', 'nota': 5},
+        {'texto': 'Caio', 'nota': 1},
+        {'texto': 'Gislaine', 'nota': 10},
+      ],
+    },
+  ];
+
+  void _responder(int nota) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _notaTotal += nota;
+      });
+    }
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _notaTotal = 0;
+    });
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder)
+            : Resultado(_notaTotal, _reiniciarQuestionario),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
